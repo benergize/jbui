08ccedf6014aeefdedd082633224d6620041760f
Benjamin Ehrlich <bensaysstuff@gmail.com>
2019-10-17 14:35:13 -0400

    Update jbui.js

--- jbui.js
+++ jbui.js
@@ -1,45 +1,39 @@
 jbui = {
-    registry: {},
-    init: function() {
+        registry: {},
+        init: function() {
 
-        Array.from(document.getElementsByClassName('jbui')).forEach(function(el) {
-            jbui.registry[el.dataset.jbuiName] = el.outerHTML;
-            el.remove();
-        });
-    },
+            Array.from(document.getElementsByClassName('jbui')).forEach(function(el) {
+                jbui.registry[el.dataset.jbuiName] = el.outerHTML;
+                el.remove();
+            });
+        },
 
-    create: function(componentToCreate, inputs) {
+        create: function(componentToCreate, inputs) {
 
-        let comp = jbui.registry[componentToCreate];
-        let newDiv = document.createElement("div");
-        newDiv.innerHTML = comp;
+            let comp = jbui.registry[componentToCreate];
+            let newDiv = document.createElement("div");
+            newDiv.innerHTML = comp;
 
-        Array.from(newDiv.getElementsByClassName('jbuiElement')).forEach(function(thisElement) {
+            Array.from(newDiv.getElementsByClassName('jbuiElement')).forEach(function(thisElement) {
 
-            for(v in inputs) { 
+                for(v in inputs) { 
 
-                if(thisElement.dataset.jbuiName === v) {
+                    if(thisElement.dataset.jbuiName === v) {
 
-                    let thisInput = inputs[v];
+                        let thisInput = inputs[v];
 
-                    for(property in thisInput) {
+                        for(property in thisInput) {
 
-                        if(property === "data" || property === "dataset") {
-
-                            for(dataProperty in thisInput[property]) {
-
-                                thisElement.dataset[dataProperty] = thisInput[property][dataProperty];
-                            }
-                            
+                            if(property === "data" || property === "dataset") { for(dataProperty in thisInput[property]) { thisElement.dataset[dataProperty] = thisInput[property][dataProperty]; } }
+                            else if(property === "style") { for(styleProperty in thisInput[property]) { thisElement.style[styleProperty] = thisInput[property][styleProperty]; } }
+                            else { thisElement[property] = thisInput[property]; }
                         }
-                        else { thisElement[property] = thisInput[property]; }
+
                     }
-
                 }
-            }
-        });
+            });
 
-        return newDiv;
+            return newDiv;
 
+        }
     }
-}
